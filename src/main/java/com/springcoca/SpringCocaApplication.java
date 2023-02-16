0f593f142b88697ba5ed85cf23cc24d20609e4dc
cocapanqi <>
2023-02-16 16:03:05 +0800

    Initial commit

---
+++ src/main/java/com/springcoca/SpringCocaApplication.java
@@ -0,0 +1,13 @@
+package com.springcoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCocaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCocaApplication.class, args);
+    }
+
+}
