96392eb528eabaf72220de432c1a12d3e339feda
cocapanqi <>
2023-02-17 18:05:28 +0800

    swagge未完成

--- mall-order/src/main/java/com/mallorder/controller/OrderController.java
+++ mall-order/src/main/java/com/mallorder/controller/OrderController.java
@@ -4,6 +4,9 @@ import com.mallcommon.api.CommonResult;
 import com.mallorder.Service.OrderService;
 import com.mallcommon.domain.Orderss;
 import com.mallcommon.dto.UserDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,47 +14,56 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@Api(tags = "OrderController")
+@Tag(name = "OrderController", description = "订单管理")
 @RequestMapping("Order")
 public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @ApiOperation("获取所有收货地址")
     @GetMapping("GetOrderAll")
     public CommonResult<List<Orderss>> GetOrderAll() {
         List<Orderss> result = orderService.GetOrderAll();
         return CommonResult.success(result);
     }
 
+    @ApiOperation("获取所有收货地址")
     @GetMapping("GetOrder/{OrderId}")
     public CommonResult<Orderss> GetOrder(@PathVariable Long OrderId) {
         Orderss result = orderService.GetOrder(OrderId);
         return CommonResult.success(result);
     }
 
+    @ApiOperation("获取所有收货地址")
     @GetMapping("GetOrderByNo/{OrderNo}")
     public CommonResult<Orderss> GetOrderByNo(@PathVariable String OrderNo) {
         Orderss result = orderService.GetOrderByNo(OrderNo);
         return CommonResult.success(result);
     }
 
+    @ApiOperation("获取所有收货地址")
     @PostMapping("OrderQuery")
     public CommonResult<List<Orderss>> OrderQuery(UserDto userDto) {
         List<Orderss> result=orderService.OrderQuery(userDto);
         return CommonResult.success(result);
     }
 
+    @ApiOperation("获取所有收货地址")
     @PostMapping("InsertOrder")
     public CommonResult<Integer> InsertOrder(Orderss order) {
         int result=orderService.InsertOrder(order);
         return CommonResult.success(result);
     }
 
+    @ApiOperation("获取所有收货地址")
     @PostMapping("UpdateOrder")
     public CommonResult<Integer> UpdateOrder(Orderss order) {
         int result= orderService.UpdateOrder(order);
         return CommonResult.success(result);
     }
 
+    @ApiOperation("获取所有收货地址")
     @RequestMapping("DeleteOrder")
     public CommonResult<Integer> DeleteOrder(Integer OrderId) {
         int result= orderService.DeleteOrder(OrderId);
