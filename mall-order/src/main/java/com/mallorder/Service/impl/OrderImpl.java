136de6062faf013e024c616e9405fefec84a73ab
cocapanqi <>
2023-02-17 16:14:57 +0800

    增加redis封装

--- mall-order/src/main/java/com/mallorder/Service/impl/OrderImpl.java
+++ mall-order/src/main/java/com/mallorder/Service/impl/OrderImpl.java
@@ -1,23 +1,40 @@
 package com.mallorder.Service.impl;
 
 
+import com.mallcommon.service.RedisService;
 import com.mallorder.Service.OrderService;
 import com.mallorder.dao.OrderDao;
 import com.mallcommon.domain.Orderss;
 import com.mallcommon.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class OrderImpl implements OrderService {
+    @Value("${redis.database}")
+    private String REDIS_DATABASE;
+
+    @Value("${redis.key.resourceList}")
+    private String REDIS_KEY_RESOURCE_LIST;
+
     @Autowired
     private OrderDao orderMapper;
-
+    @Autowired
+    private RedisService redisService;
 
     public List<Orderss> GetOrderAll() {
-        return orderMapper.GetOrderAll();
+        String keys=REDIS_DATABASE+":"+REDIS_KEY_RESOURCE_LIST+"GetOrderAll";
+        List<Orderss> result=(List<Orderss>)redisService.get(keys);
+        if (result==null){
+            result=orderMapper.GetOrderAll();
+            if(result.stream().count()>0){
+                redisService.set(keys,result,100000);
+            }
+        }
+        return result;
     }
 
     public int GetOrderCount() {
