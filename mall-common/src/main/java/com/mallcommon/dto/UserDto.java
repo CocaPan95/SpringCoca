9361dd7bba21bcf238b066a5b23d131d0dcc1aea
cocapanqi <>
2023-02-20 17:22:29 +0800

    jwt加上swagger

--- mall-common/src/main/java/com/mallcommon/dto/UserDto.java
+++
@@ -1,9 +0,0 @@
-package com.mallcommon.dto;
-
-import lombok.Data;
-
-@Data
-public class UserDto {
-    public String OrderNo;
-    public Long OrderId;
-}
--- mall-mbg/src/main/java/com/mallmbg/mapper/UmsAdminRoleRelationMapper.java
+++ mall-mbg/src/main/java/com/mallmbg/mapper/UmsAdminRoleRelationMapper.java
@@ -3,8 +3,11 @@ package com.mallmbg.mapper;
 import com.mallmbg.model.UmsAdminRoleRelation;
 import com.mallmbg.model.UmsAdminRoleRelationExample;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+@Mapper
 public interface UmsAdminRoleRelationMapper {
     long countByExample(UmsAdminRoleRelationExample example);
 
