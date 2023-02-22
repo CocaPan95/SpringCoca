404e070914adca8a09fdcd4839801d20dc4dd00e
2530470001 <2530470001@qq.com>
2023-02-22 14:51:58 +0800

    下单相关功能和rabbitmq

--- mall-portal/src/main/java/com/mallportal/MallPortalApplication.java
+++ mall-portal/src/main/java/com/mallportal/MallPortalApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages  = "com")
 public class MallPortalApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MallPortalApplication.class, args);
     }
